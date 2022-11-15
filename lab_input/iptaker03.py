eeb455b3efc3d89065b65e34c74fc531898e0174
james <jamespjones0714@gmail.com>
2022-11-15 22:33:53 +0000

    About complex lists

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Please enter your name:")
+
+    input_week = input("what day of the week is it?:")
+
+    ## print() can be given a series of objects separated by a comma
+    print(f"Hello {user_input}! Happy {input_week}!")
+
+
+
+
+main()
+
